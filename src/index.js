501b9cf2fba4648586caa09e97d3b83ddd302b84
ChrisYuneva <christina_yuneva@mail.ru>
2021-03-12 17:52:37 +0400

    feat: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array==0) {
+    return 0;
+  }
+
+  let min = null;
+  for(let i=0; i<array.length; i++){
+    if (min > array[i]) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array==0) {
+    return 0;
+  }
+  let max = null;
+  for(let i=0; i<array.length; i++){
+    if (max < array[i]) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array==0) {
+    return 0;
+  }
+  let mean = 0;
+  for(let i=0; i<array.length; i++){
+    mean += array[i];
+  }
+  return mean / array.length;
 }
